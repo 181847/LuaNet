2c19abed2d5479b9b0015aaa558788396de6dfb3
181847 <753090084@qq.com>
2017-11-27 13:08:20 +0800

    删除原有的库文件，准备更新

--- Library/MyTools/UsefulDataType.h
+++
@@ -1,7 +0,0 @@
-// this file define the useful data type such as BYTE, WORD , DWORD
-
-typedef unsigned char	BYTE;
-typedef unsigned short	WORD;
-typedef unsigned long	DWORD;
-
-typedef unsigned int UINT;
